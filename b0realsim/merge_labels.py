b9d511abf648df2dd2a262727fa5c18912753d42
Mathieu Boudreau <emb6150@gmail.com>
2024-10-17 15:30:53 -0300

    Rough initial draft of merge labels & slicer smoothing scripts

---
+++ b0realsim/merge_labels.py
@@ -0,0 +1,236 @@
+from pathlib import Path
+
+from typing import List
+import numpy.typing as npt
+
+import nibabel as nib
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def merge_labels(list_labels: List[Path], config_file=None) -> None:
+    """
+    Merges multiple tissue label volumes into a single volume, optionally remapping labels based on a configuration file.
+
+    Parameters
+    ----------
+    list_labels : List[Path]
+        List of Paths to tissue labels.
+    config : Path, optional
+        Path to a configuration file in TSV format that specifies label remapping.
+
+    Error
+    -----
+    Raises an error if any of the label files cannot be loaded or if the configuration file is invalid.
+
+    Returns
+    -------
+    None
+    """
+
+    num_files = len(list_labels)
+
+    # Get label suffixes
+    label_suffixes = []
+
+    for label_path in list_labels:
+        label_suffixes.append(get_label_suffix(label_path))
+
+    label_prefix = get_label_prefix(label_path)
+
+    print(str(num_files) + " files were provided.")
+    print("The labels provided are: ")
+    print(*label_suffixes, ", ")
+
+    # Load volumes of labels
+    volume_set = {}
+
+    for idx, label_path in enumerate(list_labels):
+        volume_set[label_suffixes[idx]] = load_volume(label_path)
+
+    print("Loaded the volumes for all the following files:")
+    print(*volume_set.keys(), ", ")
+
+    # Create new nifti volume
+    template_path = list_labels[0]  # Assuming the first label file can be used as a template
+    volume_nifti = create_volume(template_path)
+    volume = volume_nifti.get_fdata()
+
+    # Load configuration file if provided
+    if config_file is not None:
+        config_db = pd.read_csv(config_file, sep="\t", header=0)
+    else:
+        config_db = None
+
+    #for idx, key in enumerate(volume_set):
+    #    labels = volume_set[key]
+    #    suffix = label_suffixes[idx]
+    #    volume = set_labels(volume, labels, suffix, config=config_db)
+
+    volume = set_labels(volume,  volume_set[label_suffixes[0]], label_suffixes[0], config=config_db, anatomy=['body'] )
+    volume = set_labels(volume,  volume_set[label_suffixes[3]], label_suffixes[3], config=config_db)
+    volume = set_labels(volume,  volume_set[label_suffixes[2]], label_suffixes[2], config=config_db)
+    volume = set_labels(volume,  volume_set[label_suffixes[1]], label_suffixes[1], config=config_db)
+    volume = set_labels(volume,  volume_set[label_suffixes[0]], label_suffixes[0], config=config_db, anatomy=['lung left', 'lung right'] )
+    volume = set_labels(volume,  volume_set[label_suffixes[4]], label_suffixes[4], config=config_db, anatomy=['skull'] )
+    volume = set_labels(volume,  volume_set[label_suffixes[0]], label_suffixes[0], config=config_db, anatomy=['sinus', 'ear canal', 'trachea'])
+    volume = set_labels(volume,  volume_set[label_suffixes[5]], label_suffixes[5], config=config_db, anatomy=['eyes'] )
+
+    save_volume(template_path, volume)
+
+    return None
+
+
+def load_volume(nifti_path: Path) -> npt.NDArray:
+    """
+    Loads the volume of a NIfTI image as an ndarray
+
+    Parameters
+    ----------
+    nifti_path : Path
+        Path of Nifti file
+
+    Returns
+    -------
+    volume : numpy.typing.NDArray
+        Image volume
+    """
+
+    nifti = nib.load(nifti_path)
+
+    volume = nifti.get_fdata()
+
+    return volume
+
+
+def create_volume(template_path: Path) -> nib.nifti1.Nifti1Image:
+    """
+    Creates a new NIfTI volume based on a template, with all voxel values set to zero.
+
+    Parameters
+    ----------
+    template_path : Path
+        Path to the template NIfTI file.
+
+    Returns
+    -------
+    new_volume : nib.nifti1.Nifti1Image
+        New NIfTI image with the same shape and affine as the template, but with all voxel values set to zero.
+    """
+
+    nifti = nib.load(template_path)
+
+    volume = nifti.get_fdata()
+
+    volume.fill(0)
+
+    new_volume = nib.Nifti1Image(volume, nifti.affine, nifti.header)
+
+    return new_volume
+
+def save_volume(template_path: Path, volume) -> nib.nifti1.Nifti1Image:
+    """
+    Creates a new NIfTI volume based on a template, with all voxel values set to zero.
+
+    Parameters
+    ----------
+    template_path : Path
+        Path to the template NIfTI file.
+
+    Returns
+    -------
+    new_volume : nib.nifti1.Nifti1Image
+        New NIfTI image with the same shape and affine as the template, but with all voxel values set to zero.
+    """
+
+    nifti = nib.load(template_path)
+
+    new_volume = nib.Nifti1Image(volume.astype(np.int8), nifti.affine, nifti.header,  dtype=np.int8)
+    nib.save(new_volume,'foo.nii.gz')
+    return None
+
+def get_label_suffix(nifti_path: Path) -> str:
+    """
+    Extracts the suffix of the BIDS label filename, i.e., the text that follows "label" in the filename.
+
+    Parameters
+    ----------
+    nifti_path : Path
+        Path of Nifti file.
+
+    Returns
+    -------
+    file_suffix : str
+        The suffix of the BIDS label filename.
+    """
+
+    file_str = str(nifti_path)
+    file_suffix = file_str.split("label", 1)[1]
+
+    return file_suffix
+
+def get_label_prefix(nifti_path: Path) -> str:
+    """
+    Extracts the prefix of the BIDS label filename, i.e., the text that precedes "label" in the filename.
+    """
+
+    file_str = str(nifti_path)
+    file_prefix = file_str.split("label", 1)[0]
+
+    return file_prefix
+
+def set_labels(volume, labels, suffix, config=None, anatomy=None) -> str:
+    """
+    Sets the labels to a volume, remapping if configured to do so.
+
+    Parameters
+    ----------
+    volume : numpy.typing.NDArray
+        The volume to set labels in.
+    labels : numpy.typing.NDArray
+        The labels to set in the volume.
+    suffix : str
+        The suffix of the label file.
+    config : pandas.DataFrame, optional
+        Configuration DataFrame for remapping labels.
+
+    Returns
+    -------
+    file_suffix : str
+        The suffix of the BIDS label filename.
+    """
+
+    file_db = config[config['source']==suffix]
+
+    if anatomy is not None:
+        for tissue in anatomy:
+            new_label_val = int(file_db[file_db['anatomy']==tissue]["label"].values[0])
+            old_label_val = int(file_db[file_db['anatomy']==tissue]["original-label"].values[0])
+            volume[labels==old_label_val] = new_label_val 
+    else:
+        for tissue in file_db['anatomy'].tolist():
+            new_label_val = int(file_db[file_db['anatomy']==tissue]["label"].values[0])
+            old_label_val = file_db[file_db['anatomy']==tissue]["original-label"].values[0]
+            volume[labels==old_label_val] = new_label_val 
+    return volume
+
+
+def main() -> None:
+    air_tissue = Path("data/sub-amuAL_T1w_label-air_tissue.nii.gz")
+    canal_seg = Path("data/sub-amuAL_T1w_label-canal_seg.nii.gz")
+    spine_dseg = Path("data/sub-amuAL_T1w_label-spine_dseg.nii.gz")
+    brain_dseg = Path("data/sub-amuAL_T1w_label-brain_dseg.nii.gz")
+    skull = Path("data/sub-amuAL_T1w_label-skull.nii.gz")
+    eyes = Path("data/sub-amuAL_T1w_label-eyes.nii.gz")
+
+    list_labels = [air_tissue, canal_seg, spine_dseg, brain_dseg, skull, eyes]
+
+    config = Path("config/whole-body-labels.tsv")
+
+    merge_labels(list_labels, config)
+
+    return None
+
+
+if __name__ == "__main__":
+    main()
---
+++ b0realsim/slicer_scripts/save_skull.py
@@ -0,0 +1,78 @@
+import slicer
+import os
+import click
+
+@click.command()
+@click.option('-m','--main_volume_path', required=True, help='Path to the main volume')
+@click.option('-s','--segmentation_path', required=True, help='Path to the SAMSEG segmentation')
+@click.option('-o','--output_path', required=True, help='Path to the output file')
+def save_skull(main_volume_path, segmentation_path, output_path):
+    """
+
+    
+    """
+
+    print("Starting the smoothing procedure...")
+
+    # Load the main volume and segmentation
+    main_filename = main_volume_path.split('\\')[-1] # Get the filename from the path
+
+    # Remove the extension to
+    main_filename_without_extension = main_filename.split('.')[0]
+
+    print(f"Loading main volume from: {main_volume_path}")
+    slicer.util.loadVolume(main_volume_path)
+    
+    print("Main volume loaded")
+    
+    masterVolumeNode = slicer.util.getNode(main_filename_without_extension)
+    print("Main volume node obtained")
+    
+    print(f"Loading segmentation from: {segmentation_path}")
+    segmentation_node = slicer.util.loadSegmentation(segmentation_path)
+    print("Segmentation loaded")
+    
+
+    # Create segment editor to get access to effects
+    segmentEditorWidget = slicer.qMRMLSegmentEditorWidget()
+    segmentEditorWidget.setMRMLScene(slicer.mrmlScene)
+    segmentEditorNode = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLSegmentEditorNode")
+    segmentEditorWidget.setMRMLSegmentEditorNode(segmentEditorNode)
+    segmentEditorWidget.setSegmentationNode(segmentation_node)
+    segmentEditorWidget.setMasterVolumeNode(masterVolumeNode)
+
+    # Define the segment to select
+    segmentEditorNode.SetSelectedSegmentID('Segment_165')
+
+    # List of segments to keep
+    segmentsToKeep = ['Segment_165'] # Skull
+
+    segmentation = segmentation_node.GetSegmentation()
+    # List to store segment IDs that need to be removed
+    segmentsToRemove = []
+
+    # Iterate through all segment IDs in the segmentation
+    segmentIDs = [segmentation.GetNthSegmentID(i) for i in range(segmentation.GetNumberOfSegments())]
+
+    for segmentID in segmentIDs:
+        # Get the segment name
+        segmentName = segmentation.GetSegment(segmentID).GetName()
+        
+        # Check if the segment name is not in the list of segments to keep
+        if segmentName not in segmentsToKeep:
+            # Add the segment ID to the list of segments to remove
+            segmentsToRemove.append(segmentID)
+
+    # Remove segments that are not in the list of segments to keep
+    for segmentID in segmentsToRemove:
+        segmentation.RemoveSegment(segmentID)
+
+    print("Smoothing procedure completed. Saving the output...")
+    # Export segmentation to a labelmap
+    labelmapVolumeNode = slicer.mrmlScene.AddNewNodeByClass('vtkMRMLLabelMapVolumeNode')
+    slicer.modules.segmentations.logic().ExportVisibleSegmentsToLabelmapNode(segmentation_node, labelmapVolumeNode, masterVolumeNode)
+    slicer.util.saveNode(labelmapVolumeNode, output_path)
+
+
+if __name__ == '__main__':  
+    save_skull()  
